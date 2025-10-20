76a43cf68602aa6038d0bfbb7736dc05aedaf7f2
Maria Lordwill <MariaLordwill@pneumatic.app>
2025-10-20 13:14:33 +0400

    43154 fix(test): update import to 'import * as React from React' for test compatibility

--- frontend/src/public/components/Workflows/WorkflowsTablePage/WorkflowsTable/Columns/Cells/SystemDefaultColumns.tsx
+++ frontend/src/public/components/Workflows/WorkflowsTablePage/WorkflowsTable/Columns/Cells/SystemDefaultColumns.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Column } from 'react-table';
 import { TableColumns } from '../../types';
 import { Skeleton } from '../../../../../UI/Skeleton';
