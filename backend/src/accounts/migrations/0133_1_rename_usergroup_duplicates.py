88912ad3091b9bcc44aa94ff2b4556d21c44e027
Eugen Birkenfeld <EBirkenfeld@pneumatic.app>
2025-10-24 23:53:07 +0500

    43266 fix(migration): fix 0133_1_rename_usergroup_duplicates.py

--- backend/src/accounts/migrations/0133_1_rename_usergroup_duplicates.py
+++ backend/src/accounts/migrations/0133_1_rename_usergroup_duplicates.py
@@ -3,28 +3,6 @@
 from django.db import migrations
 
 
-def rename_usergroup_duplicates(apps, schema_editor):
-    from django.db import connection
-    
-    update_query = """
-        UPDATE accounts_usergroup
-        SET name = CONCAT(accounts_usergroup.name, ' ', (numbered_groups.copy_number - 1))
-        FROM (
-          SELECT
-            id,
-            ROW_NUMBER() OVER (PARTITION BY name, account_id ORDER BY name) AS copy_number
-          FROM accounts_usergroup
-          WHERE is_deleted IS FALSE
-          ORDER BY account_id
-        ) AS numbered_groups
-        WHERE numbered_groups.copy_number > 1
-        AND accounts_usergroup.id=numbered_groups.id
-    """
-    
-    with connection.cursor() as cursor:
-        cursor.execute(update_query)
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -32,8 +10,20 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(
-            rename_usergroup_duplicates,
-            reverse_code=migrations.RunPython.noop
+        migrations.RunSQL(
+            """
+            UPDATE accounts_usergroup
+            SET name = CONCAT(accounts_usergroup.name, ' ', (numbered_groups.copy_number - 1))
+            FROM (
+              SELECT
+                id,
+                ROW_NUMBER() OVER (PARTITION BY name, account_id ORDER BY name) AS copy_number
+              FROM accounts_usergroup
+              WHERE is_deleted IS FALSE
+              ORDER BY account_id
+            ) AS numbered_groups
+            WHERE numbered_groups.copy_number > 1
+            AND accounts_usergroup.id=numbered_groups.id;
+            """
         ),
     ]
