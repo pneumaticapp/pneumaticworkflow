8d90f3da26a917de430546e4996b3a5e19f1a1b8
Eugen Birkenfeld <EBirkenfeld@pneumatic.app>
2025-10-27 14:03:42 +0500

    43266 fix(migration): rename migrations

--- backend/src/accounts/migrations/0135_add_usergroup_name_account_unique.py
+++ backend/src/accounts/migrations/0135_add_usergroup_name_account_unique.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0133_1_rename_usergroup_duplicates'),
+        ('accounts', '0134_rename_usergroup_duplicates'),
     ]
 
     operations = [
