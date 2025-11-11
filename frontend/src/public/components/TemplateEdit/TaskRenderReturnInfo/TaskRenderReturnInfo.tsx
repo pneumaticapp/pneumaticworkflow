2471cab5124f90da8a73618c6767d55dd5ab617a
Maria Lordwill <MariaLordwill@pneumatic.app>
2025-11-11 11:09:18 +0400

    42423 fix(AI): correct 'Use Template' button

--- frontend/src/public/components/TemplateEdit/TaskRenderReturnInfo/TaskRenderReturnInfo.tsx
+++ frontend/src/public/components/TemplateEdit/TaskRenderReturnInfo/TaskRenderReturnInfo.tsx
@@ -11,21 +11,19 @@ interface ITaskRenderReturnInfoProps {
 export const TaskRenderReturnInfo = ({ task: { revertTask }, onClick }: ITaskRenderReturnInfoProps) => {
   const { formatMessage } = useIntl();
 
-  return (
-    revertTask && (
-      <span
-        className={styles['task-view__return-info']}
-        onClick={onClick}
-        role="button"
-        tabIndex={0}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter' || e.key === ' ') {
-            onClick();
-          }
-        }}
-      >
-        {formatMessage({ id: 'templates.return-to.minimize' })}
-      </span>
-    )
-  );
+  return revertTask ? (
+    <span
+      className={styles['task-view__return-info']}
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          onClick();
+        }
+      }}
+    >
+      {formatMessage({ id: 'templates.return-to.minimize' })}
+    </span>
+  ) : null;
 };
