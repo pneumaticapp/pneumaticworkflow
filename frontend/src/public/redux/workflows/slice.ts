8b66be75cf800166eaf274e1858f3184d8101761
Maria Lordwill <MariaLordwill@pneumatic.app>
2025-12-05 13:53:47 +0400

    refactor(workflows redux): migrate to RTK - create slice.ts

---
+++ frontend/src/public/redux/workflows/slice.ts
@@ -0,0 +1,382 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IStoreWorkflows, IWorkflowsList } from '../../types/redux';
+import { IKickoff } from '../../types/template';
+import {
+  EWorkflowsLoadingStatus,
+  EWorkflowsLogSorting,
+  EWorkflowsSorting,
+  EWorkflowsStatus,
+  EWorkflowsView,
+  ITemplateFilterItem,
+  IWorkflowLogItem,
+  IWorkflowsSettings,
+  TTemplateStepCounter,
+} from '../../types/workflow';
+import { isDesktop } from '../../utils/media';
+import { getWorkflowViewStorageState, setWorkflowViewStorageState } from '../../utils/workflows/filters';
+import { isArrayWithItems, isObjectChanged } from '../../utils/helpers';
+
+const initialWorkflowsList = {
+  count: -1,
+  offset: 0,
+  items: [],
+} as IWorkflowsList;
+
+export const initialWorkflowsFilters: IWorkflowsSettings['values'] = {
+  statusFilter: EWorkflowsStatus.Running,
+  templatesIdsFilter: [],
+  stepsIdsFilter: [],
+  performersIdsFilter: [],
+  performersGroupIdsFilter: [],
+  workflowStartersIdsFilter: [],
+};
+
+const initialWorkflowEdit = {
+  workflow: {
+    name: '',
+    kickoff: { description: '', fields: [] } as IKickoff,
+  },
+  isWorkflowNameEditing: false,
+  isKickoffEditing: false,
+  isKickoffSaving: false,
+  isWorkflowNameSaving: false,
+};
+
+export const initialState: IStoreWorkflows = {
+  workflowsList: initialWorkflowsList,
+  workflowsLoadingStatus: EWorkflowsLoadingStatus.Loaded,
+  workflow: null,
+  isWorkflowLoading: false,
+  workflowEdit: initialWorkflowEdit,
+  workflowLog: {
+    items: [] as IWorkflowLogItem[],
+    isCommentsShown: true,
+    isOnlyAttachmentsShown: false,
+    sorting: EWorkflowsLogSorting.New,
+    isOpen: false,
+    isLoading: false,
+    workflowId: null,
+  },
+  workflowsSearchText: '',
+  workflowsSettings: {
+    view: getWorkflowViewStorageState() || (isDesktop() ? EWorkflowsView.Table : EWorkflowsView.Grid),
+    areFiltersChanged: false,
+    sorting: EWorkflowsSorting.DateDesc,
+    values: initialWorkflowsFilters,
+    selectedFields: [],
+    presets: [],
+    templateList: {
+      items: [],
+      isLoading: false,
+    },
+    counters: {
+      workflowStartersCounters: [],
+      performersCounters: [],
+    },
+    lastLoadedTemplateIdForTable: null,
+  },
+  WorkflowsTuneViewModal: {
+    isOpen: false,
+  },
+};
+
+const checkFiltersChanged = (state: IStoreWorkflows) => {
+  return isObjectChanged(initialWorkflowsFilters, state.workflowsSettings.values);
+};
+
+const updateWorkflowsFilterValue = <T extends keyof IWorkflowsSettings['values']>(
+  state: IStoreWorkflows,
+  filter: T,
+  value: IWorkflowsSettings['values'][T],
+) => {
+  state.workflowsSettings.values[filter] = value;
+  state.workflowsSettings.areFiltersChanged = checkFiltersChanged(state);
+};
+
+const workflowsSlice = createSlice({
+  name: 'workflows',
+  initialState,
+  reducers: {
+    openWorkflowLogPopup: (state, action) => {
+      state.workflowLog.isOpen = true;
+      state.workflowLog.workflowId = action.payload.workflowId;
+    },
+    closeWorkflowLogPopup: (state) => {
+      state.workflowLog.isOpen = false;
+      state.workflowLog.isOnlyAttachmentsShown = false;
+    },
+    changeWorkflowsSorting: (state, action) => {
+      state.workflowsSettings.sorting = action.payload;
+    },
+    changeWorkflow: (state, action) => {
+      state.workflow = action.payload;
+    },
+    changeWorkflowsList: (state, action) => {
+      state.workflowsList = action.payload;
+      state.workflowsLoadingStatus = isArrayWithItems(action.payload.items)
+        ? EWorkflowsLoadingStatus.Loaded
+        : EWorkflowsLoadingStatus.EmptyList;
+    },
+    changeWorkflowLog: (state, action) => {
+      state.workflowLog = { ...state.workflowLog, ...action.payload };
+    },
+    updateWorkflowLogItem: (state, action) => {
+      const index = state.workflowLog.items.findIndex((item) => item.id === action.payload.id);
+
+      if (index !== -1) {
+        state.workflowLog.items[index] = action.payload;
+      } else {
+        state.workflowLog.items = [action.payload, ...state.workflowLog.items];
+      }
+    },
+    changeWorkflowLogViewSettings: (state, action) => {
+      state.workflowLog.isCommentsShown = action.payload.comments;
+      state.workflowLog.isOnlyAttachmentsShown = action.payload.isOnlyAttachmentsShown;
+      state.workflowLog.sorting = action.payload.sorting;
+    },
+    changeWorkflowsSearchText: (state, action) => {
+      state.workflowsLoadingStatus = EWorkflowsLoadingStatus.LoadingList;
+      state.workflowsSearchText = action.payload;
+    },
+    setWorkflowIsLoading: (state, action) => {
+      state.isWorkflowLoading = action.payload;
+    },
+    loadWorkflowsList: (state, action) => {
+      state.workflowsLoadingStatus =
+        action.payload === 0 ? EWorkflowsLoadingStatus.LoadingList : EWorkflowsLoadingStatus.LoadingNextPage;
+    },
+    loadWorkflow: (state, action) => {
+      state.workflowLog.workflowId = action.payload;
+    },
+    loadWorkflowsListFailed: (state) => {
+      state.workflowsLoadingStatus = EWorkflowsLoadingStatus.Loaded;
+    },
+    loadFilterTemplates: (state) => {
+      state.workflowsSettings.templateList.isLoading = true;
+    },
+    loadFilterTemplatesSuccess: (state, action) => {
+      state.workflowsSettings.templateList.isLoading = false;
+      state.workflowsSettings.templateList.items = action.payload.map(
+        (template: ITemplateFilterItem, index: number) => ({
+          ...template,
+          steps: state.workflowsSettings.templateList.items[index]?.steps || [],
+          areStepsLoading: false,
+        }),
+      );
+    },
+    loadFilterTemplatesFailed: (state) => {
+      state.workflowsSettings.templateList.isLoading = false;
+    },
+    loadFilterSteps: (state, action) => {
+      const templateIndex = state.workflowsSettings.templateList.items.findIndex((template) => {
+        return template.id === action.payload.templateId;
+      });
+
+      if (templateIndex === -1) {
+        return;
+      }
+
+      state.workflowsSettings.templateList.items[templateIndex].areStepsLoading = true;
+    },
+    loadFilterStepsSuccess: (state, action) => {
+      const templateIndex = state.workflowsSettings.templateList.items.findIndex((template) => {
+        return template.id === action.payload.templateId;
+      });
+      if (templateIndex === -1) {
+        return;
+      }
+
+      state.workflowsSettings.templateList.items[templateIndex].areStepsLoading = false;
+      state.workflowsSettings.templateList.items[templateIndex].steps = action.payload.steps;
+    },
+    loadFilterStepsFailed: (state, action) => {
+      const templateIndex = state.workflowsSettings.templateList.items.findIndex((template) => {
+        return template.id === action.payload.templateId;
+      });
+
+      if (templateIndex === -1) {
+        return;
+      }
+
+      state.workflowsSettings.templateList.items[templateIndex].areStepsLoading = false;
+    },
+    setFilterStatus: (state, action) => {
+      updateWorkflowsFilterValue(state, 'statusFilter', action.payload);
+    },
+    setFilterTemplate: (state, action) => {
+      updateWorkflowsFilterValue(state, 'templatesIdsFilter', action.payload);
+    },
+    setFilterTemplateSteps: (state, action) => {
+      updateWorkflowsFilterValue(state, 'stepsIdsFilter', action.payload);
+    },
+    setFilterPerformers: (state, action) => {
+      updateWorkflowsFilterValue(state, 'performersIdsFilter', action.payload);
+    },
+    setFilterPerformersGroup: (state, action) => {
+      updateWorkflowsFilterValue(state, 'performersGroupIdsFilter', action.payload);
+    },
+    setFilterWorkflowStarters: (state, action) => {
+      updateWorkflowsFilterValue(state, 'workflowStartersIdsFilter', action.payload);
+    },
+
+    setIsEditWorkflowName: (state, action) => {
+      state.workflowEdit.isWorkflowNameEditing = action.payload;
+    },
+    setIsEditKickoff: (state, action) => {
+      state.workflowEdit.isKickoffEditing = action.payload;
+    },
+    setIsSavingWorkflowName: (state, action) => {
+      state.workflowEdit.isWorkflowNameSaving = action.payload;
+    },
+    setIsSavingKickoff: (state, action) => {
+      state.workflowEdit.isKickoffSaving = action.payload;
+    },
+    setWorkflowEdit: (state, action) => {
+      state.workflowEdit.workflow = action.payload;
+    },
+    resetWorkflows: (state) => {
+      state.workflowsList = initialWorkflowsList;
+      state.workflowLog.isOpen = false;
+    },
+    clearFilters: (state) => {
+      state.workflowsSettings.values = initialWorkflowsFilters;
+      state.workflowsSettings.areFiltersChanged = false;
+      state.workflowsSettings.sorting = EWorkflowsSorting.DateDesc;
+      state.workflowsLoadingStatus = EWorkflowsLoadingStatus.LoadingList;
+      state.workflowsSearchText = '';
+    },
+    clearAppFilters: (state) => {
+      state.workflowsSettings.values = initialWorkflowsFilters;
+      state.workflowsSettings.areFiltersChanged = false;
+      state.workflowsSettings.sorting = EWorkflowsSorting.DateDesc;
+      state.workflowsLoadingStatus = EWorkflowsLoadingStatus.LoadingList;
+      state.workflowsSearchText = '';
+    },
+    applyFilters: (state) => {
+      state.workflowsLoadingStatus = EWorkflowsLoadingStatus.LoadingList;
+    },
+    clearWorkflow: (state) => {
+      state.workflow = null;
+      state.workflowEdit = initialWorkflowEdit;
+    },
+    setCurrentPerformersCounters: (state, action) => {
+      state.workflowsSettings.counters.performersCounters = action.payload;
+    },
+    setWorkflowStartersCounters: (state, action) => {
+      state.workflowsSettings.counters.workflowStartersCounters = action.payload;
+    },
+    setWorkflowsTemplateStepsCounters: (state, action: PayloadAction<TTemplateStepCounter[]>) => {
+      state.workflowsSettings.templateList.items.forEach((template) => {
+        template.steps.forEach((step) => {
+          const stepCountInfo = action.payload.find(({ templateTaskId }) => templateTaskId === step.id);
+          step.count = stepCountInfo ? stepCountInfo.workflowsCount : 0;
+        });
+      });
+    },
+    patchWorkflowInList: (state, action) => {
+      const newListItems = state.workflowsList.items.map((workflow) => {
+        if (workflow.id !== action.payload.workflowId) {
+          return workflow;
+        }
+
+        return { ...workflow, ...action.payload.changedFields };
+      });
+
+      state.workflowsList.items = newListItems;
+    },
+    patchWorkflowDetailed: (state, action) => {
+      if (state.workflow?.id === action.payload.workflowId) {
+        state.workflow = { ...state.workflow, ...action.payload.changedFields };
+      }
+    },
+    removeWorkflowFromList: (state, action) => {
+      const { items: workflowsItems, count: workflowsCount, offset: workflowsOffset } = state.workflowsList;
+
+      const newWorkflows = workflowsItems.filter((workflow) => workflow.id !== action.payload.workflowId);
+      const isWorkflowRemoved = newWorkflows.length !== workflowsItems.length;
+
+      if (!isWorkflowRemoved) {
+        return;
+      }
+
+      const newWorkflowsList = {
+        items: newWorkflows,
+        count: workflowsCount - 1,
+        offset: workflowsOffset - 1,
+      };
+
+      state.workflowsList = newWorkflowsList;
+    },
+    setWorkflowsView: (state, action) => {
+      setWorkflowViewStorageState(action.payload);
+      state.workflowsSettings.view = action.payload;
+    },
+    openTuneViewModal: (state) => {
+      state.WorkflowsTuneViewModal.isOpen = true;
+    },
+    closeTuneViewModal: (state) => {
+      state.WorkflowsTuneViewModal.isOpen = false;
+    },
+    setFilterSelectedFields: (state, action) => {
+      state.workflowsSettings.selectedFields = action.payload;
+    },
+    setLastLoadedTemplateId: (state, action) => {
+      state.workflowsSettings.lastLoadedTemplateIdForTable = action.payload;
+    },
+    setWorkflowsPresetsRedux: (state, action) => {
+      state.workflowsSettings.presets = action.payload;
+    },
+  },
+});
+
+export const {
+  openWorkflowLogPopup,
+  closeWorkflowLogPopup,
+  changeWorkflowsSorting,
+  changeWorkflow,
+  changeWorkflowsList,
+  changeWorkflowLog,
+  updateWorkflowLogItem,
+  changeWorkflowLogViewSettings,
+  changeWorkflowsSearchText,
+  setWorkflowIsLoading,
+  loadWorkflowsList,
+  loadWorkflow,
+  loadWorkflowsListFailed,
+  loadFilterTemplates,
+  loadFilterTemplatesSuccess,
+  loadFilterTemplatesFailed,
+  loadFilterSteps,
+  loadFilterStepsSuccess,
+  loadFilterStepsFailed,
+  setFilterStatus,
+  setFilterTemplate,
+  setFilterTemplateSteps,
+  setFilterPerformers,
+  setFilterPerformersGroup,
+  setFilterWorkflowStarters,
+  setIsEditWorkflowName,
+  setIsEditKickoff,
+  setIsSavingWorkflowName,
+  setIsSavingKickoff,
+  setWorkflowEdit,
+  resetWorkflows,
+  clearFilters,
+  clearAppFilters,
+  applyFilters,
+  clearWorkflow,
+  setCurrentPerformersCounters,
+  setWorkflowStartersCounters,
+  setWorkflowsTemplateStepsCounters,
+  patchWorkflowInList,
+  patchWorkflowDetailed,
+  removeWorkflowFromList,
+  setWorkflowsView,
+  openTuneViewModal,
+  closeTuneViewModal,
+  setFilterSelectedFields,
+  setLastLoadedTemplateId,
+  setWorkflowsPresetsRedux,
+} = workflowsSlice.actions;
+
+export default workflowsSlice.reducer;
