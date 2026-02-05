2e716fe99ed23019bffa2661d1dde4b89c2e6cb5
Maria Lordwill <MariaLordwill@pneumatic.app>
2026-02-05 15:47:06 +0400

    44397 test(users): update snapshots

--- frontend/src/public/components/Team/Users/Users.tsx
+++ frontend/src/public/components/Team/Users/Users.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
+import  { useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 
