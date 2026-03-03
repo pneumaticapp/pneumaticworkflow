111cd4fc1350a4fe495590a12e2edca8e07a0534
joseph.vreeken <joseph.vreeken@pneumatic.app>
2026-03-03 17:31:06 +0500

    43724 fix(triggers): correct update related SearchContent record for TaskTemplate

--- backend/src/processes/migrations/0242_add_new_search_content.py
+++ backend/src/processes/migrations/0242_add_new_search_content.py
@@ -227,7 +227,10 @@ class Migration(migrations.Migration):
                     )
                 )
                 ON CONFLICT (workflow_id, task_id, event_id, task_field_id, template_id, task_template_id) WHERE is_deleted = FALSE
-                DO UPDATE SET content = setweight(to_tsvector('pg_catalog.english', prepare_search_content(new.name)), 'D');
+                DO UPDATE SET content = (
+                      setweight(to_tsvector('pg_catalog.english', prepare_search_content(new.name)), 'D') ||
+                      setweight(to_tsvector('pg_catalog.english', prepare_search_content(new.description)), 'D')
+                    );
                 RETURN new;
             END;
             $BODY$ LANGUAGE plpgsql;
