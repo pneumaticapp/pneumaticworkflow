1dc8be2190c26123381a4c2271646e8b7564963f
Maria Lordwill <MariaLordwill@pneumatic.app>
2026-08-14 16:01:54 +0400

    48741 fix(fieldset-name): fix fieldset name line wrapping in template editor

--- frontend/src/public/components/TemplateEdit/TaskOutputFlow/MergedOutputRows.tsx
+++ frontend/src/public/components/TemplateEdit/TaskOutputFlow/MergedOutputRows.tsx
@@ -79,7 +79,7 @@ export function MergedOutputRows({
           >
             <div className={kickoffStyles['kick-off-input__field']}>
               <div className={styles['flow__fieldset-header']}>
-                {formatMessage({ id: 'fieldsets.header-label' })}: {name}
+                {formatMessage({ id: 'fieldsets.header-label' })}:{'\u00a0'}{name}
               </div>
               <FieldsetEditorTitle
                 apiNameBinding={apiNameBinding}
