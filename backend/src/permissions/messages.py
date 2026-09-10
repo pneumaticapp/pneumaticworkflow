7aaaa9f055ae9780c21bcf3c420632a452744d5a
Eugen Birkenfeld <EBirkenfeld@pneumatic.app>
2026-09-10 15:25:55 +0500

    48017 feat(i18n): translate permissions and manager performer errors

--- backend/src/permissions/messages.py
+++ backend/src/permissions/messages.py
@@ -2,6 +2,6 @@ from django.utils.text import format_lazy
 from django.utils.translation import gettext_lazy as _
 
 MSG_PM_0001 = lambda obj_type: format_lazy(
-    _('Unsupported object type: "{obj_type}"'),
+    _('"{obj_type}" is not a supported object type.'),
     obj_type=obj_type,
 )
